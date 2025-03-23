51243d3f24a6efe3d9923319a2e3c2f1bcaa1bcb
Kave McDaniel <kave.mdi@gmail.com>
2025-03-23 21:47:05 +0100

    Add Navbar component with logo and theme toggle functionality

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,16 @@
+import HamburgerMenu from "./icons/hamburger";
+import Logo from "./icons/logo";
+import ThemeToggle from "./icons/themeToggle";
+
+export function Navbar() {
+    return (
+        <nav className={"pt-6 px-8 flex justify-between items-center"}>
+            <Logo />
+            <div className="flex items-center">
+                <ThemeToggle />
+                <HamburgerMenu />
+            </div>
+
+        </nav>
+    );
+}
