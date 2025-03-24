4b54e6b5bfc5a1fbd6e5b79a1209720bd334e46f
Kave McDaniel <kave.mdi@gmail.com>
2025-03-24 13:21:45 +0100

    Add ArrowTop and ArrowTopRight SVG icons and components

---
+++ src/app/components/icons/arrow-top-right.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ArrowTopRight() {
+  return (
+    <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Icons/Arrow">
+    <path id="Arrow" fillRule="evenodd" clipRule="evenodd" d="M8.88001 3.19303L13.6512 3.73279C13.944 3.76591 14.2081 4.03011 14.2413 4.32289L14.781 9.09405C14.8141 9.38683 14.6037 9.59733 14.3109 9.56421C14.0181 9.53109 13.7539 9.26689 13.7208 8.97411L13.3258 5.48279L3.48999 15.3186L2.65546 14.4841L12.4913 4.64826L8.99995 4.25329C8.70717 4.22017 8.44297 3.95597 8.40985 3.66319C8.37673 3.37041 8.58723 3.15991 8.88001 3.19303Z" fill="white"/>
+    </g>
+    </svg>
+  )
+}
